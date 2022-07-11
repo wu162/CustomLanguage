3b9399365b9d543be0b661653a2ff8d74cd2a2ce
GitHub Action <action@github.com>
2022-07-11 14:46:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/wu162/customlanguage/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wu162/customlanguage/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wu162.customlanguage.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wu162.customlanguage.MyBundle
 
 class MyApplicationService {
 
